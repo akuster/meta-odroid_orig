52e3a8e78ccc2acef1d6556bf45e88007f36ee0d
Armin Kuster <akuster808@gmail.com>
2015-05-01 06:48:45 -0700

    odroid ux3: initial booting kernel
    Signed-off-by: Armin Kuster <akuster808@gmail.com>

--- recipes-bsp/u-boot/u-boot-odroidux3-2012.07/sd_fusing.sh
+++ recipes-bsp/u-boot/u-boot-odroidux3-2012.07/sd_fusing.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 Samsung Electronics Co., Ltd.
+# Copyright (C) 2013 Samsung Electronics Co., Ltd.
 #              http://www.samsung.com/
 #
 # This program is free software; you can redistribute it and/or modify
@@ -22,49 +22,36 @@ else
     exit 0
 fi
 
-if [ -d /sys/block/${1##*/}boot0 ]; then
-    echo "$1 is an eMMC card, disabling ${1##*/}boot0 ro"
-    if ! echo 0 > /sys/block/${1##*/}boot0/force_ro; then
-	echo "Enabling r/w for $1boot0 failed"
-	exit 1
-    fi
-    emmc=1
-fi
-
 ####################################
 # fusing images
 
-if [ -n "$emmc" ]; then
-    signed_bl1_position=0
-    bl2_position=30
-    uboot_position=62
-    tzsw_position=2110
-    device=$1boot0
-else
-    signed_bl1_position=1
-    bl2_position=31
-    uboot_position=63
-    tzsw_position=2111
-    device=$1
-fi
+signed_bl1_position=1
+bl2_position=31
+uboot_position=63
+tzsw_position=719
+env_position=1231
 
 #<BL1 fusing>
 echo "BL1 fusing"
-sudo dd iflag=dsync oflag=dsync if=./bl1.HardKernel of=$device seek=$signed_bl1_position
+dd iflag=dsync oflag=dsync if=./bl1.bin.hardkernel of=$1 seek=$signed_bl1_position
 
 #<BL2 fusing>
 echo "BL2 fusing"
-sudo dd iflag=dsync oflag=dsync if=./bl2.HardKernel of=$device seek=$bl2_position
+dd iflag=dsync oflag=dsync if=./bl2.bin.hardkernel of=$1 seek=$bl2_position
 
 #<u-boot fusing>
 echo "u-boot fusing"
-sudo dd iflag=dsync oflag=dsync if=../u-boot.bin of=$device seek=$uboot_position
+dd iflag=dsync oflag=dsync if=./u-boot.bin.hardkernel of=$1 seek=$uboot_position
 
 #<TrustZone S/W fusing>
 echo "TrustZone S/W fusing"
-sudo dd iflag=dsync oflag=dsync if=./tzsw.HardKernel of=$device seek=$tzsw_position
+dd iflag=dsync oflag=dsync if=./tzsw.bin.hardkernel of=$1 seek=$tzsw_position
+
+#<u-boot env default>
+echo "u-boot env erase"
+dd iflag=dsync oflag=dsync if=/dev/zero of=$1 seek=$env_position count=32 bs=512
 
 ####################################
 #<Message Display>
 echo "U-boot image is fused successfully."
-echo "Eject $1 and insert it again."
+echo "Eject SD card and insert it again."
--- recipes-bsp/u-boot/u-boot-odroidux3_2012.07.bb
+++ recipes-bsp/u-boot/u-boot-odroidux3_2012.07.bb
@@ -3,14 +3,14 @@ require u-boot-hardkernel.inc
 DESCRIPTION = "u-boot bootloader for Odroid UX3 devices supported by the hardkernel product"
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}-${PV}:"
-#DEFAULT_PREFERENCE = "-1" 
 
 SRCREV = "d80b05d5624ecba99c15ee2a7b3f59ebf6f8f1e8"
 BRANCH = "odroidxu3-v2012.07"
 
-SRC_URI += "  \
+SRC_URI += "\
     file://sd_fusing.sh \
-    file://bl1.HardKernel \
-    file://bl2.HardKernel \
-    file://tzsw.HardKernel \
-    "
+    file://boot.ini \
+    file://bl1.bin.hardkernel \
+    file://bl2.bin.hardkernel \
+    file://tzsw.bin.hardkernel \
+    file://u-boot.bin.hardkernel"
